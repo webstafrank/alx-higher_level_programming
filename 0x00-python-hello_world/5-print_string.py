fbf1593d4a094e6969fb6879c64abb3eadbf16b0
webstafrank <franklyndenish@gmail.com>
2024-03-15 07:54:32 -0700

    print string

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -2,5 +2,5 @@
 str = "Holberton School"
 # printing str 3 times followed by a new line
 # followed by the 9 first chars and a new line
-print(f"{str * 3}")
-print(f"{str :.9}")
+print(f"{str * 3}\n{str :.9}")
+
