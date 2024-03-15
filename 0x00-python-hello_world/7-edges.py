abe0b445587c166901223ab531b1996264af5887
webstafrank <franklyndenish@gmail.com>
2024-03-15 08:26:08 -0700

    "copy - cut - paste"

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# word_first_3 should contain the first 3 letters of the variable word
+#word_last_2 should contain the last 2 letters of the variable word
+#middle_word should contain the value of the variable word without the first and last letters
+word_first_3 = word[:3]
+middle_word = word[1:-1]
+word_last_2 = word[-2:]
 print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
+print(f"Last 2 letters: {word_last_2}")
+
