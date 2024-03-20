8cd60038f1d16e4584578463ee32e651dd9f73f3
webstafrank <franklyndenish@gmail.com>
2024-03-20 13:37:47 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for iteret in str:
+        x = iteret
+        if ord(x) >= 97 and ord(x) <= 122:
+            x = chr(ord(iteret) - 32)
+        print("{}".format(x), end='')
+    print()
