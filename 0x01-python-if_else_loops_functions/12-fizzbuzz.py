1ee10d994f012e1d5f10ad0d510c6e59616310e4
webstafrank <franklyndenish@gmail.com>
2024-03-21 07:42:15 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,11 @@
 #!/bin/bash/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FIZZBUZZ", end=' ')
+        elif num % 3 == 0:
+            print("FIZZ", end=' ')
+        elif num % 5 == 0:
+            print("BUZZ", end=' ')
+        else:
+            print(num, end=' ')
