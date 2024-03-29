4847ee86a02d0bf9a1ad362a0ca4df89994740e4
webstafrank <franklyndenish@gmail.com>
2024-03-29 13:39:34 -0700

    Lists of lists \= Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print("{:}".format(i))
+    if matrix:
+        for i in matrix:
+            for x in i:
+                print("{:d}".format(x), end=' ' if x != i[-1] else ' ')
+            print()
---
+++ 0x03-python-data_structures/7-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a[]
+    tuple_b[]
+    if Tuple_a < 2 and tuple_b < 2:
+        return (0)
+    if 
+
