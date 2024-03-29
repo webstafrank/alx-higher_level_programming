5f0640ce376c330ba5cb95ba02020892f8617a88
webstafrank <franklyndenish@gmail.com>
2024-03-29 14:03:24 -0700

    Tupple addition

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a[]
-    tuple_b[]
-    if Tuple_a < 2 and tuple_b < 2:
-        return (0)
-    if 
+add_tuple = __import__('7-add_tuple').add_tuple
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
 
