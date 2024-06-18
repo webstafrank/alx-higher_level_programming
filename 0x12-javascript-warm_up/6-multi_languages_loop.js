085714a09db297f893839ba6a98fb1096e58adea
webstafrank <franklyndenish@gmail.com>
2024-06-17 18:43:32 -0700

    loop in arrays

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Create an array of strings
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// looping to iterate through the array and print each message
+for (const message of messages)
+{
+  console.log(message);
+}
