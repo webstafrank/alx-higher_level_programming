cdd6699cb22d593b23e2a7d4e66a96897ab148e1
webstafrank <franklyndenish@gmail.com>
2024-06-17 17:59:14 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Getting  arguments passed to the script, except first two elements
+const args = process.argv.slice(2);
+
+// Check the number of arguments and print the appropriate message
+if (args.length === 0) 
+{
+  console.log("No argument");
+} else if (args.length === 1) 
+{
+  console.log("Argument found");
+} else 
+{
+  console.log("Arguments found");
+}
