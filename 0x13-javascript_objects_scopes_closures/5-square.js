3852114f9fadbe7db6fe8117367debd7c2230716
webstafrank <franklyndenish@gmail.com>
2024-06-18 19:21:38 -0700

    square zero

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size); // Call the constructor of Rectangle with size for width and height
+  }
+}
+
+module.exports = Square;
