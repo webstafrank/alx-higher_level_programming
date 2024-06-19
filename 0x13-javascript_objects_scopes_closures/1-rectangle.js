0415cfb0e5ec24f9b225ede0fab5c63ebccc7d19
webstafrank <franklyndenish@gmail.com>
2024-06-18 18:47:35 -0700

    rectangle 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//creating a class and defining its requirements
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
