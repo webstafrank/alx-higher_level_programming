714703a06eb41fa7dcfffa97c68a8cc73560efe6
webstafrank <franklyndenish@gmail.com>
2024-07-08 08:02:59 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Checking if a URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Storing the URL from the argument
-URL=$1
-
-# Sending a request to the URL and gettin the size of the response body in bytes
-body_size=$(curl -s -o /dev/null -w "%{size_download}" "$URL")
-
-# Display the size
-echo "$body_size"
+# bash script that takes a URL, sends a request to it an displays the size of the body of the response
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
 
