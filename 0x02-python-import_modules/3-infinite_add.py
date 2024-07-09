be6d4c5241e1faeaa43184f6b041a373367e389a
webstafrank <franklyndenish@gmail.com>
2024-07-09 16:57:38 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python33
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    # adding from index 1 to exclude the script name
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    
+    print(total)
