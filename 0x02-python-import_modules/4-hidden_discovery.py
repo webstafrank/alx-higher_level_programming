674cd5b9ff81180a4d5885e935bb44cb51ae30ed
webstafrank <franklyndenish@gmail.com>
2024-07-09 17:01:30 -0700

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,24 @@
-#!/usr/bin
+#!/usr/bin/python3
+
+import dis
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 1:
+        sys.exit(1)
+    
+    with open("hidden_4.pyc", "rb") as file:
+        code = file.read()
+    
+    instructions = dis.get_instructions(code)
+    names = set()
+    
+    for instr in instructions:
+        if instr.opname == 'STORE_NAME' and not instr.argval.startswith('__'):
+            names.add(instr.argval)
+    
+    sorted_names = sorted(names)
+    
+    for name in sorted_names:
+        print(name)
+
